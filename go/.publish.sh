0db8772cd880ce5c99e638efbfc6b89e4520bd68
zephinzer <dev@joeir.net>
2018-01-28 23:54:24 +0800

    updated publishing pipeline for `go`

--- go/.publish.sh
+++ go/.publish.sh
@@ -5,15 +5,10 @@ TAG="${REPOSITORY_URL}-next";
 TAG_LATEST="${REPOSITORY_URL}-latest";
 
 VERSIONS=$(docker run --entrypoint="version-info" ${TAG});
-VERSION_NODE=$(printf "${VERSIONS}" | grep -e 'node:' | cut -f 2 -d ':');
-VERSION_NODE_MAJOR=$(printf "${VERSIONS}" | grep -e 'node_major:' | cut -f 2 -d ':');
-VERSION_NODE_MINOR=$(printf "${VERSIONS}" | grep -e 'node_minor:' | cut -f 2 -d ':');
-VERSION_NODE_PATCH=$(printf "${VERSIONS}" | grep -e 'node_patch:' | cut -f 2 -d ':');
-VERSION_NPM=$(printf "${VERSIONS}" | grep npm | cut -f 2 -d ':');
-VERSION_YARN=$(printf "${VERSIONS}" | grep yarn | cut -f 2 -d ':');
-EXISTENCE_TAG="node-${VERSION_NODE}_npm-${VERSION_NPM}_yarn-${VERSION_YARN}";
+VERSION_GO=$(printf "${VERSIONS}" | grep -e 'go:' | cut -f 2 -d ':');
+EXISTENCE_TAG="go-${VERSION_GO}";
 EXISTENCE_REPO_URL="${REPOSITORY_URL}-${EXISTENCE_TAG}";
-NODE_VERSION_REPO_URL="${REPOSITORY_URL}-${VERSION_NODE}";
+GO_VERSION_REPO_URL="${REPOSITORY_URL}-${VERSION_GO}";
 
 printf "Checking existence of [${EXISTENCE_REPO_URL}]...";
 $(docker pull ${EXISTENCE_REPO_URL}) && EXISTS=$?;
@@ -25,9 +20,9 @@ if [ "${EXISTS}" != "0" ] || [ -z "${COMMIT_MESSAGE##*'[force build]'*}" ]; then
   printf "Pushing [${EXISTENCE_REPO_URL}]... ";
   docker tag ${TAG} ${EXISTENCE_REPO_URL};
   docker push ${EXISTENCE_REPO_URL};
-  printf "Pushing [${NODE_VERSION_REPO_URL}]... ";
-  docker tag ${TAG} ${NODE_VERSION_REPO_URL};
-  docker push ${NODE_VERSION_REPO_URL};
+  printf "Pushing [${GO_VERSION_REPO_URL}]... ";
+  docker tag ${TAG} ${GO_VERSION_REPO_URL};
+  docker push ${GO_VERSION_REPO_URL};
 else
   printf "[${EXISTENCE_REPO_URL}] found. Skipping push.\n";
   echo exists;
